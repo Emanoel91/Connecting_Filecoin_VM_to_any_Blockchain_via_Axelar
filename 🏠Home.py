85ba1430e8204951a2e7b7ac2d4c131ffcd5b471
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-31 02:03:39 +0330

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 # --- Page Config: Tab Title & Icon ---
 st.set_page_config(
     page_title="Connecting Filecoin VM to any Blockchain via Axelar",
-    page_icon="https://img.cryptorank.io/coins/filecoin1602761898881.png",
+    page_icon="https://pbs.twimg.com/profile_images/1869486848646537216/rs71wCQo_400x400.jpg",
     layout="wide"
 )
 
@@ -66,16 +66,8 @@ st.markdown(
             <a href="https://www.axelar.network/" target="_blank">https://www.axelar.network/</a>
         </div>
         <div style="display: flex; align-items: center; gap: 10px;">
-            <img src="https://axelarscan.io/logos/accounts/squid.svg" alt="Squid" style="width:20px; height:20px;">
-            <a href="https://www.squidrouter.com/" target="_blank">https://www.squidrouter.com/</a>
-        </div>
-        <div style="display: flex; align-items: center; gap: 10px;">
-            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968958.png" alt="X" style="width:20px; height:20px;">
+            <img src="https://axelarscan.io/logos/logo.png" alt="X" style="width:20px; height:20px;">
             <a href="https://x.com/axelar" target="_blank">https://x.com/axelar</a>
-        </div>
-        <div style="display: flex; align-items: center; gap: 10px;">
-            <img src="https://cdn-icons-png.flaticon.com/512/5968/5968958.png" alt="X" style="width:20px; height:20px;">
-            <a href="https://x.com/squidrouter" target="_blank">https://x.com/squidrouter</a>
         </div>
     </div>
     """,
