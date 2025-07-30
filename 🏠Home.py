18362de71d62d5c9b6372b16a855aa080ace14a4
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-31 02:04:45 +0330

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -63,11 +63,11 @@ st.markdown(
     <div style="font-size: 16px;">
         <div style="display: flex; align-items: center; gap: 10px;">
             <img src="https://axelarscan.io/logos/logo.png" alt="Axelar" style="width:20px; height:20px;">
-            <a href="https://www.axelar.network/" target="_blank">https://www.axelar.network/</a>
+            <a href="https://www.axelar.network/" target="_blank">Axelar Website</a>
         </div>
         <div style="display: flex; align-items: center; gap: 10px;">
             <img src="https://axelarscan.io/logos/logo.png" alt="X" style="width:20px; height:20px;">
-            <a href="https://x.com/axelar" target="_blank">https://x.com/axelar</a>
+            <a href="https://x.com/axelar" target="_blank">Axelar X Account</a>
         </div>
     </div>
     """,
