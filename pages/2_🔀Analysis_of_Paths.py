24001641aad65a4d3902c3f18be8fc133c39c501
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-31 02:27:56 +0330

    Update 2_🔀Analysis_of_Paths.py

--- pages/2_🔀Analysis_of_Paths.py
+++ pages/2_🔀Analysis_of_Paths.py
@@ -1,1 +1,46 @@
+import streamlit as st
+import pandas as pd
+import snowflake.connector
+import plotly.express as px
+import plotly.graph_objects as go
+
+# --- Page Config: Tab Title & Icon ---
+st.set_page_config(
+    page_title="Connecting Filecoin VM to any Blockchain via Axelar",
+    page_icon="https://pbs.twimg.com/profile_images/1869486848646537216/rs71wCQo_400x400.jpg",
+    layout="wide"
+)
+
+st.title("🔎Analysis of Paths")
+
+st.info(
+    "📊Charts initially display data for a default time range. Select a custom range to view results for your desired period."
+
+)
+
+st.info(
+    "⏳On-chain data retrieval may take a few moments. Please wait while the results load."
+)
+
+# --- Snowflake Connection ---
+conn = snowflake.connector.connect(
+    user=st.secrets["snowflake"]["user"],
+    password=st.secrets["snowflake"]["password"],
+    account=st.secrets["snowflake"]["account"],
+    warehouse="SNOWFLAKE_LEARNING_WH",
+    database="AXELAR",
+    schema="PUBLIC"
+)
+
+# --- Time Frame & Period Selection ---
+timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
+start_date = st.date_input("Start Date", value=pd.to_datetime("2024-01-01"))
+end_date = st.date_input("End Date", value=pd.to_datetime("2025-07-31"))
+
+# --- Query Functions ---------------------------------------------------------------------------------------
+
+
+# --- Load Data ----------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------------------------
 
