5b5ea41493c351154b7163a97580f9d7bade8883
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-31 02:24:25 +0330

    Update 1_🔎Overview_of_Transfers.py

--- pages/1_🔎Overview_of_Transfers.py
+++ pages/1_🔎Overview_of_Transfers.py
@@ -1,1 +1,46 @@
+import streamlit as st
+import pandas as pd
+import snowflake.connector
+import plotly.express as px
+import plotly.graph_objects as go
+
+# --- Page Config: Tab Title & Icon ---
+st.set_page_config(
+    page_title="Squid: Chain Swaps",
+    page_icon="https://axelarscan.io/logos/accounts/squid.svg",
+    layout="wide"
+)
+
+st.title("🔎Overview of Transfers")
+
+st.info(
+    "📊Charts initially display data for a default time range. Select a custom range to view results for your desired period."
+
+)
+
+st.info(
+    "⏳On-chain data retrieval may take a few moments. Please wait while the results load."
+)
+
+# --- Snowflake Connection ---
+conn = snowflake.connector.connect(
+    user=st.secrets["snowflake"]["user"],
+    password=st.secrets["snowflake"]["password"],
+    account=st.secrets["snowflake"]["account"],
+    warehouse="SNOWFLAKE_LEARNING_WH",
+    database="AXELAR",
+    schema="PUBLIC"
+)
+
+# --- Time Frame & Period Selection ---
+timeframe = st.selectbox("Select Time Frame", ["month", "week", "day"])
+start_date = st.date_input("Start Date", value=pd.to_datetime("2022-01-01"))
+end_date = st.date_input("End Date", value=pd.to_datetime("2025-06-01"))
+
+# --- Query Functions ---------------------------------------------------------------------------------------
+
+
+# --- Load Data ----------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------------------------------
 
